d2417a1bbd8cedec3ccc113fd1e9e86d0dbfc93f
Francisco Bernal <Francisco.Bernal@ust.com>
2021-07-19 17:40:50 -0500

    Busqueda de doc

--- src/app/_usys/partials/content/dashboards/dashboard1/dashboard1.component.ts
+++ src/app/_usys/partials/content/dashboards/dashboard1/dashboard1.component.ts
@@ -1,7 +1,8 @@
  import { Component, OnInit } from '@angular/core';
-
+ import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import { Documento } from 'src/app/_usys/core/models/documento.model';
 import { DocumentoService } from '../../../../../_usys/core/services/modules/documento.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-dashboard1',
@@ -10,9 +11,12 @@ import { DocumentoService } from '../../../../../_usys/core/services/modules/doc
 export class Dashboard1Component implements OnInit {
 
 public listaDocumento: Documento[];
+public urlDocumentoLista;
 
 constructor(
-  public documentoService: DocumentoService
+  public documentoService: DocumentoService,
+  private modalService: NgbModal,
+  private dom: DomSanitizer
 ) { }
 
 
@@ -29,5 +33,17 @@ constructor(
     this.documentoService.obtenerDocumentos(idOrganizacion, filtro, apartirDe, mostrar).subscribe(json => {
      this.listaDocumento = json as Documento[];
     });
+  }
+
+
+  abrirVistaDocumento(content, url) {
+    this.urlDocumentoLista = this.dom.bypassSecurityTrustResourceUrl(`http://docs.google.com/gview?url=${url}&embedded=true`); 
+    this.modalService.open(content, {
+        size: 'lg'
+    });
+  }
+
+  public descargarDocumento(url){
+    window.location.href= url;
   }
 }
